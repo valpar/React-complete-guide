7487ca5e9082c92f2b9de4acfb520776e9401464
Valmar <valmarparts@gmail.com>
2024-07-15 15:44:04 +0300

    Update js refresher starter 1/3 done

---
+++ code/02 JS Refresher/code/starter/assets/scripts/app.js
@@ -0,0 +1,48 @@
+//import { apiKey } from './util.js';
+//import { apiKey, abc } from './util.js';
+//import * as Util from './util.js';
+
+//console.log(Util.apiKey);
+
+// variables, values and operators
+//variables are data containers
+//Reusability and readability
+
+// let userMessage = 'Hello world!';
+// console.log(userMessage);
+
+// const userName = 'Max';
+// userName = 'Mary'; //error
+// console.log(userName);
+
+// console.log(10 + 6); // addition
+// console.log(10 - 6); // subtraction
+// console.log(10 * 6); // multiplication
+// console.log(10 / 6); // division
+// console.log(10 % 6); // modulus
+// console.log(10 ** 6); // exponentiation
+// console.log(10 == 6); // equality
+// console.log(10 === 6); // strict equality
+// console.log(10 != 6); // inequality
+// console.log(10 >= 6); // greater than or equal to
+// console.log(10 <= 6); // less than or equal to
+
+// functions
+// function greetUser(userName, message = 'Hello') {
+//   // console.log(userName + ' says ' + message);
+
+//   return 'Hi , I am ' + userName + '. ' + message + '.';
+// }
+
+// greetUser('Max', 'Good morning');
+// const greeting = greetUser('Max', 'Good morning');
+// console.log(greeting);
+
+// arrow functions
+
+// export default (userName, message) => {
+//   console.log('Hello');
+//   return userName + ' says ' + message;
+// };
+
+// Objects and classes
---
+++ code/02 JS Refresher/code/starter/assets/scripts/util.js
@@ -0,0 +1,4 @@
+//export let apiKey = 'fsdfsdfdsffsfsdfsdfsd1344123';
+// export default 'fsdfsdfdsffsfsdfsdfsd1344123';
+// export let apiKey = 'sdasdsddd';
+// export let abc = 'k3242234322';
