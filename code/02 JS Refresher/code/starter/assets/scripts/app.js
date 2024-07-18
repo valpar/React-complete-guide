9a416930e4de1bd68ba148a7ca8fa24b7c9d9315
valmar parts <valmarparts@gmail.com>
2024-07-18 13:16:40 +0300

    Finish js refresher

--- code/02 JS Refresher/code/starter/assets/scripts/app.js
+++ code/02 JS Refresher/code/starter/assets/scripts/app.js
@@ -46,3 +46,137 @@
 // };
 
 // Objects and classes
+
+// const user = {
+//   name: 'Max',
+//   age: 34,
+//   greet: function () {
+//     console.log(
+//       'Hi, I am ' + this.name + '.' + ' I am ' + this.age + ' years old.'
+//     );
+//   },
+// };
+
+// console.log(user.greet());
+
+// class Person {
+//   constructor(name, age) {
+//     this.name = name;
+//     this.age = age;
+//   }
+
+//   greet() {
+//     console.log(
+//       'Hi, I am ' + this.name + '.' + ' I am ' + this.age + ' years old.'
+//     );
+//   }
+// }
+
+// const user1 = new Person('Max', 34);
+// console.log(user1.greet());
+
+// Arrays and Array methods
+
+// const hobbies = ['Sports', 'Cooking', 'Swimming', 'Reading', 'Cycling'];
+// console.log(hobbies[3]);
+
+// hobbies.push('Singing');
+// console.log(hobbies);
+
+// const index = hobbies.findIndex((hobby) => hobby === 'Reading');
+// console.log(index);
+
+// const funHobbies = hobbies.map((hobby) => ({
+//   text: hobby,
+// })); //map
+// console.log(funHobbies);
+
+// const firstName = userNameData[0];
+// const lastName = userNameData[1];
+// const [firstName, lastName] = ['Jarl', 'Windhelm'];
+// console.log(firstName);
+// console.log(lastName);
+// console.log()
+
+// const { name, age } = {
+//   name: 'Vlad',
+//   age: 34,
+// };
+
+// spread operator
+// const hobbies = ['Sports', 'Cooking', 'Swimming', 'Reading', 'Cycling'];
+// const user = {
+//   name: 'Jepser',
+//   age: 34,
+// };
+
+// const newHobbies = ['Climbing'];
+
+// const mergedHobbies = [...hobbies, ...newHobbies];
+// console.log(mergedHobbies);
+
+// const extendedUser = { isAdmin: true, ...user };
+// console.log(extendedUser);
+
+// Control structures
+
+//const password = prompt('Enter your password');
+
+// if (password === '123456') {
+//   console.log('Password is correct');
+// } else {
+//   console.log('Password is incorrect');
+// }
+
+// const hobbies = ['Sports', 'Cooking', 'Swimming', 'Reading', 'Cycling'];
+
+// for (let hobby of hobbies) {
+//   console.log(hobby);
+// }
+
+// const list = document.querySelector('ul');
+// list.remove();
+
+// using functions as values
+
+// function handleTimeout() {
+//   console.log('Timeout!');
+// }
+
+// const handleTimeout2 = () => {
+//   console.log('Timeout ...again!');
+// };
+
+// setTimeout(handleTimeout, 2000);
+// setTimeout(handleTimeout2, 3000);
+// setTimeout(() => {
+//   console.log('Timeout one more time!');
+// }, 4000);
+
+// function greeter(greetFn) {
+//   greetFn();
+// }
+
+// greeter(() => console.log('Hello!'));
+
+//functions inside of functions
+// function init() {
+//   function greet() {
+//     console.log('Hello');
+//   }
+
+//   greet();
+// }
+
+// init();
+
+//refrence vs primitive values
+
+// let userMessage = 'Hello world!';
+// userMessage = 'Hello';
+
+// const hobbies = ['Sports', 'Cooking', 'Swimming', 'Reading', 'Cycling'];
+// hobbies.push('Singing');
+// console.log(hobbies);
+
+// const message = 'potato';
