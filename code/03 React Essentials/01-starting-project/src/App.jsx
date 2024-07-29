c36a32ba67f757b80346099bfef682c68d4c5f15
Valmar <valmarparts@gmail.com>
2024-07-29 10:55:48 +0300

    Add props and alternative prop syntaxes

--- code/03 React Essentials/01-starting-project/src/App.jsx
+++ code/03 React Essentials/01-starting-project/src/App.jsx
@@ -1,4 +1,6 @@
 import reactImg from './assets/react-core-concepts.png';
+import componentImg from './assets/components.png';
+import { CORE_CONCEPTS } from './data';
 
 const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
 
@@ -7,7 +9,8 @@ function genRandomInt(max) {
 }
 
 function Header() {
-  const description = reactDescriptions[genRandomInt(2)];
+  const description =
+    reactDescriptions[genRandomInt(reactDescriptions.length - 1)];
 
   return (
     <header>
@@ -21,11 +24,34 @@ function Header() {
   );
 }
 
+function CoreConcept({ title, description, image }) {
+  return (
+    <li>
+      <img src={image} alt={title} />
+      <h3>{title}</h3>
+      <p>{description}</p>
+    </li>
+  );
+}
+
 function App() {
   return (
     <div>
       <Header />
       <main>
+        <section id='core-concepts'>
+          <h2>Core Concepts</h2>
+          <ul>
+            <CoreConcept
+              title={CORE_CONCEPTS[0].title}
+              description={CORE_CONCEPTS[0].description}
+              image={CORE_CONCEPTS[0].image}
+            />
+            <CoreConcept {...CORE_CONCEPTS[1]} />
+            <CoreConcept {...CORE_CONCEPTS[2]} />
+            <CoreConcept {...CORE_CONCEPTS[3]} />
+          </ul>
+        </section>
         <h2>Time to get started!</h2>
       </main>
     </div>
